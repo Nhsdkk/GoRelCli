b210e465da32a8ac0d024b6c40470904898694ef
Nhsdkk <nikittsev.anton@gmail.com>
2023-11-17 23:17:56 +0300

    feat: create async file creation

--- generate/GoRelGeneratedFileInterface.go
+++ generate/GoRelGeneratedFileInterface.go
@@ -1,7 +1,10 @@
 package generate
 
+import "sync"
+
 type GoRelGeneratedFileInterface interface {
 	Create(object ObjectUnionType, enumNames []string, modelNames []string, projectName string, projectPath string) error
 	WriteFS() error
+	WriteFSAsync(c chan error, syncGroup *sync.WaitGroup)
 	Log()
 }
